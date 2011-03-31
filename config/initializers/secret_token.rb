19a67b1e61575268f38b482dcf530d1302fda09b
Justin Fedick <justin.fedick@gmail.com>
2011-03-30 23:21:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventcalculator::Application.config.secret_token = '414b90529f16e28eebabecb9ee66d1ab579a13fdecc963eb45a18420804e55bc3b1aa2f283185bd39ebe71ae316ccc333b04d0ab2644e38845f59dfd79fac9ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Eventcalculator::Application.config.session_store :cookie_store, :key => '_eventcalculator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Eventcalculator::Application.config.session_store :active_record_store
