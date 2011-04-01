e1ec1e33dd1088d8daa866b228714ee1b35188ec
Justin Fedick <justin.fedick@gmail.com>
2011-04-01 16:53:29 -0400

    fix error when random number is 0

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,110 +1,110 @@
 class PagesController < ApplicationController
 
   def go
-    chance = 100
-    random_number = rand(100)
+    chance = 99
+    random_number = rand(99)
     if random_number < chance
-      random_number_2 = rand(10)
+      random_number_2 = rand(9)
       case random_number_2
-       when 1
+       when 0
          results = " in region 1"
-         random_number_3 = rand (10)
-         if random_number_3 == 1
+         random_number_3 = rand (9)
+         if random_number_3 == 0
            results = results + " in section A."
          else
            results = results + " in section B."
          end
-         random_number_4 = rand(10)
+         random_number_4 = rand(9)
+         results = "Event #{random_number_4} happened" + results
+       when 1
+         results = " in region 2"
+         random_number_3 = rand (9)
+         if random_number_3 === 0..2
+           results = results + " in section A."
+         else
+           results = results + " in section B."
+         end
+         random_number_4 = rand(9)
          results = "Event #{random_number_4} happened" + results
        when 2
-         results = " in region 2"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..3
-           results = results + " in section A."
-         else
-           results = results + " in section B."
-         end
-         random_number_4 = rand(10)
-         results = "Event #{random_number_4} happened" + results
-       when 3
          results = " in region 3"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..2
+         random_number_3 = rand (9)
+         if random_number_3 === 0..1
            results = results + " in section A."
          else
            results = results + " in section B."
          end
-         random_number_4 = rand(10)
+         random_number_4 = rand(9)
          results = "Event #{random_number_4} happened" + results
-       when  4
+       when  3
          results = " in region 4"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..8
+         random_number_3 = rand (9)
+         if random_number_3 === 0..7
            results = results + " in section A."
          else
            results = results + " in section B."
          end
-         random_number_4 = rand(10)
+         random_number_4 = rand(9)
+         results = "Event #{random_number_4} happened" + results
+       when 4
+         results = " in region 5"
+         random_number_3 = rand (9)
+         if random_number_3 === 0..4
+           results = results + " in section A."
+         else
+           results = results + " in section B."
+         end
+         random_number_4 = rand(9)
          results = "Event #{random_number_4} happened" + results
        when 5
-         results = " in region 5"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..5
+         results = " in region 6"
+         random_number_3 = rand (9)
+         if random_number_3 === 0..3
            results = results + " in section A."
          else
            results = results + " in section B."
          end
-         random_number_4 = rand(10)
+         random_number_4 = rand(9)
          results = "Event #{random_number_4} happened" + results
        when 6
-         results = " in region 6"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..4
+         results = " in region 7"
+         random_number_3 = rand (9)
+         if random_number_3 === 0..3
            results = results + " in section A."
          else
            results = results + " in section B."
          end
-         random_number_4 = rand(10)
+         random_number_4 = rand(9)
          results = "Event #{random_number_4} happened" + results
        when 7
-         results = " in region 7"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..4
+         results = " in region 8"
+         random_number_3 = rand (9)
+         if random_number_3 === 0..6
            results = results + " in section A."
          else
            results = results + " in section B."
          end
-         random_number_4 = rand(10)
+         random_number_4 = rand(9)
          results = "Event #{random_number_4} happened" + results
        when 8
-         results = " in region 8"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..7
+         results = " in region 9"
+         random_number_3 = rand (9)
+         if random_number_3 === 0..4
            results = results + " in section A."
          else
            results = results + " in section B."
          end
-         random_number_4 = rand(10)
+         random_number_4 = rand(9)
          results = "Event #{random_number_4} happened" + results
        when 9
          results = " in region 9"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..5
+         random_number_3 = rand (9)
+         if random_number_3 === 0..4
            results = results + " in section A."
          else
            results = results + " in section B."
          end
-         random_number_4 = rand(10)
-         results = "Event #{random_number_4} happened" + results
-       when 10
-         results = " in region 10"
-         random_number_3 = rand (10)
-         if random_number_3 === 1..5
-           results = results + " in section A."
-         else
-           results = results + " in section B."
-         end
-         random_number_4 = rand(10)
+         random_number_4 = rand(9)
          results = "Event #{random_number_4} happened" + results
        else
          results = "Error"
